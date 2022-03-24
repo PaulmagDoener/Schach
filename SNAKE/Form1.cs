e61451cd8bf10cfb6364dd9deb03cdd7e32ad279
Paul Doehlert <padoe@outlook.de>
2022-03-24 01:16:46 +0100

    hey

--- SNAKE/Form1.cs
+++ SNAKE/Form1.cs
@@ -12,9 +12,28 @@ namespace SNAKE
 {
     public partial class Form1 : Form
     {
+        public readonly PictureBox[,] Spielfeld = new PictureBox[8, 8];
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PictureBox pictureBox2 = new PictureBox();
+
+
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 100);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            pictureBox2.BackColor = Color.Red;
+            Controls.Add(pictureBox2);
+        }
+
+       
+        
     }
 }
